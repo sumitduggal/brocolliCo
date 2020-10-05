f54993321fe89a403359a4c2e0b5a1776b3761b0
Sumit Duggal <sumit@coinjar.com>
2020-10-05 22:36:35 +1100

    added navbar sticky

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,15 +4,17 @@ type NavBarProps = {
   openModal: () => void;
 };
 
-export const NavBar: React.FC<NavBarProps> = ({ openModal }) => <div className="border-b border-solid border-gray-300">
-  <nav className="container mx-auto px-5 sm:px-0 h-20 flex items-center justify-between">
-    <a href="/">
-      <h1 className="text-3xl uppercase font-sansita text-green-900">
-        Brocolli & co
-      </h1>
-    </a>
-    <button onClick={openModal} className="btn btn-small">
-      Request Invite
-    </button>
-  </nav>
-</div>
+export const NavBar: React.FC<NavBarProps> = ({ openModal }) => (
+  <div className="sticky top-0 border-b border-solid border-gray-300">
+    <nav className="container mx-auto sticky top-0 px-5 sm:px-0 h-20 flex items-center justify-between">
+      <a href="/">
+        <h1 className="text-3xl uppercase font-sansita text-green-900">
+          Brocolli & co
+        </h1>
+      </a>
+      <button onClick={openModal} className="btn btn-small">
+        Request Invite
+      </button>
+    </nav>
+  </div>
+);
